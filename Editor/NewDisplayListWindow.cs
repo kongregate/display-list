38ae3ec6aaa1f317f9ac52478aefb399ebae63f3
David LeGare <dlegare.1001@gmail.com>
2019-07-16 23:50:34 -0500

    Find all types implementing IDisplayElement

--- Editor/NewDisplayListWindow.cs
+++ Editor/NewDisplayListWindow.cs
@@ -1,9 +1,16 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
 public class NewDisplayListWindow : EditorWindow
 {
+    private readonly static Type DISPLAY_ELEMENT_TYPE = typeof(IDisplayElement<>);
+
+    private List<DisplayElementType> _displayElementTypes;
+
     private string _className;
     private string _viewType;
     private string _dataType;
@@ -21,7 +28,40 @@ public class NewDisplayListWindow : EditorWindow
     [MenuItem("Assets/Create/Display List Script", priority = 81)]
     public static void CreateDisplayList()
     {
-        _ = GetWindow<NewDisplayListWindow>();
+        var window = ScriptableObject.CreateInstance<NewDisplayListWindow>();
+        window.ShowModalUtility();
+    }
+
+    private void Awake()
+    {
+        _displayElementTypes = AppDomain
+            .CurrentDomain
+            .GetAssemblies()
+            .SelectMany(assembly => assembly.GetTypes())
+            .Where(type =>
+            {
+                return type
+                    .GetInterfaces()
+                    .Where(iface => iface.IsGenericType)
+                    .Any(iface => DISPLAY_ELEMENT_TYPE.IsAssignableFrom(iface.GetGenericTypeDefinition()));
+            })
+            .Select(displayType =>
+            {
+                var dataTypes = displayType
+                    .GetInterfaces()
+                    .Where(iface => iface.GetGenericTypeDefinition() == DISPLAY_ELEMENT_TYPE)
+                    .Select(iface => iface.GetGenericArguments()[0])
+                    .ToList();
+                return new DisplayElementType
+                {
+                    DisplayType = displayType,
+                    DataTypes = dataTypes,
+                };
+            })
+            .ToList();
+
+        var displayElements = _displayElementTypes.Select(type => $"{type.DisplayType.FullName} ({string.Join(", ", type.DataTypes.Select(dataType => dataType.FullName))})");
+        Debug.Log($"Found {_displayElementTypes.Count} display element types:\n{string.Join("\n", displayElements)}");
     }
 
     private void OnGUI()
@@ -54,7 +94,7 @@ public class NewDisplayListWindow : EditorWindow
     private static string FindSelectedDirectory()
     {
         var path = "Assets";
-        foreach (var obj in Selection.GetFiltered(typeof(Object), SelectionMode.Assets))
+        foreach (var obj in Selection.GetFiltered(typeof(UnityEngine.Object), SelectionMode.Assets))
         {
             path = AssetDatabase.GetAssetPath(obj);
             if (File.Exists(path))
@@ -65,5 +105,11 @@ public class NewDisplayListWindow : EditorWindow
         }
 
         return path;
+    }
+
+    private struct DisplayElementType
+    {
+        public Type DisplayType;
+        public List<Type> DataTypes;
     }
 }
